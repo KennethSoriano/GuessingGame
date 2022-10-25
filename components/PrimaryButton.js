920defa007ff102c934082393ff637d169e958b3
Kenneth Soriano <Kennethsoriano14017@gmail.com>
2022-10-25 08:36:38 -0700

    Styling for Android & IOS

---
+++ components/PrimaryButton.js
@@ -0,0 +1,13 @@
+import { View, Text } from "react-native"
+
+function PrimaryButton(props) {
+    return (
+        <View>
+            <Text>
+                {props.children}
+            </Text>
+        </View>
+    )
+}
+
+export default PrimaryButton
