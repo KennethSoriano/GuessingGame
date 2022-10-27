7e5e96189481cef8a5ce28782cb728737203a60e
Kenneth Soriano <Kennethsoriano14017@gmail.com>
2022-10-26 20:24:29 -0700

    Hnadling user input & showing alert

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -9,8 +9,21 @@ function StartGameScreen() {
         setEnteredNumber(enteredText);
     }
 
+    function resetInputHandler() {
+        setEnteredNumber('');
+    }
+
     function confirmInputHandler() {
-        
+        const chosenNumber = parseInt(enteredNumber);
+
+        if(isNaN(chosenNumber) || chosenNumber <= 0 || chosenNumber > 99){
+            Alert.alert(
+                'Invalid number!', 
+                'Number has to be a number between 1 and 99.', 
+                [{ text: 'Okay', style: 'destructive', onPress: resetInputHandler}]
+                );
+            return;
+        }
     }
 
     return (
@@ -26,7 +39,7 @@ function StartGameScreen() {
             />
             <View style={styles.buttonsContainer}>
                 <View style={styles.buttonContainer}>
-                    <PrimaryButton>Reset</PrimaryButton>
+                    <PrimaryButton onPress={resetInputHandler}>Reset</PrimaryButton>
                 </View>
                 <View style={styles.buttonContainer}>
                     <PrimaryButton onPress={confirmInputHandler}>Confirm</PrimaryButton>
