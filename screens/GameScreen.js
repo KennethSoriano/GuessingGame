26b18ee315cea4f509dc15516174805a5ea12a7b
Kenneth Soriano <Kennethsoriano14017@gmail.com>
2022-11-02 20:48:56 -0700

    title component

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,16 +1,16 @@
-import { Text, View, StyleSheet } from "react-native";
+import { Text, View, StyleSheet, SafeAreaView } from "react-native";
+import Title from "../components/Title";
 
 function GameScreen() {
     return (
         <View style={styles.screen}>
             <Text>Opponent's Guess</Text>
-            {Guess}
+            <Title>Opponent's Guess</Title>
             <View>
                 <Text>Higher or Lower</Text>
-                {+
-                -}
+        
             </View>
-            {<View>Log Rounds</View>}
+          
         </View>
     )
 }
@@ -20,6 +20,6 @@ export default GameScreen;
 const styles = StyleSheet.create({
     screen: {
         flex: 1,
-        padding: 12
-    }
+        padding: 24
+    },
 })
